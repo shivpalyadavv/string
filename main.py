2db6e710aeb70e5bd85bf3829191214e06f7687f
shivpalyadavv <shivpaly2@gmail.com>
2022-09-25 09:42:56 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+String = "Welcome to My Space"
+print("Creating String: ")
+print(String)
+
+# printing first character
+print("\nFirst characters of string is: ")
+print(String[0])
+
+#Printing ;last character
+print("\nLast character of string is: ")
+print(String[-1])
